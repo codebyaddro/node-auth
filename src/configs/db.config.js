cf43aed7ec82fff62ec803b385eb727731148954
codebyaddro <codebyaddro@gmail.com>
2026-06-07 13:21:21 +0600

    config file setup

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const config = require("./config");
 
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(config.MONGODB_URI);
         console.log("Database connected");
     } catch (error) {
         console.log(`Database connection failed. Error: ${error}`);
